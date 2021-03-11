ae5e6049e0f6dd8a49518db05bc0ae460319dd30
MohammadWaqas908 <m.waqas7375@gmail.com>
2021-03-11 23:41:38 +0500

    practice didMount & didUpdate method

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,42 +1,41 @@
 import React,{Component} from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody,CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody,CardTitle } from 'reactstrap';
 
 class DishDetail extends Component{
-    constructor(props){
-        super(props);
-        
+    componentDidMount(){
+        console.log('DishDetail Component componentDidMount invoked');
     }
+
+    componentDidUpdate(){
+        console.log('DishDetail Component componentDidUpdate invoked');
+    }
+
     renderDish(dish) {
-        if (dish != null)
-            return(
-                <Card>
-                    <CardImg top src={dish.image} alt={dish.name} />
-                    <CardBody>
-                      <CardTitle>{dish.name}</CardTitle>
-                      <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        else
-            return(
-                <div></div>
-            );
+        return(
+            <Card>
+                <CardImg top src={dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        );
     }
-    renderComments(array) {
-        if (array.length != 0) {
+    renderComments(comments) {
+        if (comments !=null) {
             return (
                 <div>
                     <h4>Comments</h4>
-                    {array.map(comment => (
-                        <ul className="list-unstyled">
+                    <ul className="list-unstyled">
+                        {comments.map((comment) => (
                             <li>
                                 <p>{comment.comment}</p>
                                 <p>-- {comment.author} , 
                                 {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', 
                                 day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
                             </li>
-                        </ul>
-                    ))}
+                        ))}
+                    </ul>
                 </div>
             );
         }
@@ -49,32 +48,24 @@ class DishDetail extends Component{
     }
     render(){
 
-        let dish;
-        if (this.props.selectedDish) {
-            dish = this.renderDish(this.props.selectedDish);
-        } else {
-            dish = <div></div>
-        }
-        let dishcomments;
-        if (this.props.selectedDish) {
-            dishcomments = this.renderComments(this.props.selectedDish.comments);
-        } else {
-            dishcomments = <div></div>
-        }
+        console.log('DishDetail Component render invoked');
 
-
+        if (this.props.dish) {
         return(
             <div className="container">
                 <div className="row">
                   <div  className="col-12 col-md-5 m-1">
-                    {dish}
+                    {this.renderDish(this.props.dish)}
                   </div>
                   <div  className="col-12 col-md-5 m-1">
-                    {dishcomments}
+                    {this.renderComments(this.props.dish.comments)}
                   </div>
                 </div>
             </div>
         );
+        } else {
+            return( <div></div>);
+        }
     }
 
 }
